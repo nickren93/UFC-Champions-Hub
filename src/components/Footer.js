89dfa55c16e839fb0280f2e8d0b42126e2639557
Sitong <nickren93@gmail.com>
2026-01-17 14:32:59 -0500

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,89 @@
-import './Footer.css'
+import "./Footer.css";
 
-function Footer(){
+function Footer() {
+  return (
+    <footer id="footer">
+      <div>
+        <h3>Resources</h3>
+        <p>
+          <a
+            href="https://www.ufc.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            UFC Official Website
+          </a>
+          <br />
+          <a
+            href="https://www.espn.com/mma/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ESPN MMA
+          </a>
+          <br />
+          <a
+            href="https://www.youtube.com/@ufc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            UFC YouTube Channel
+          </a>
+        </p>
+      </div>
 
-    return(
-        <footer id="footer">
-        <div>
-            <h3>Links</h3>
-            <p>
-                <a href="#" target="_blank">UFC Official Website</a><br/>
-                <a href="#" target="_blank">ESPN+</a><br/>
-                <a href="#" target="_blank">YouTube Page</a>
-            </p>
-        </div>
+      <div>
+        <h3>Follow UFC</h3>
+        <p>
+          <a
+            href="https://www.youtube.com/@ufc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            YouTube
+          </a>
+          <br />
+          <a
+            href="https://www.facebook.com/ufc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+          <br />
+          <a
+            href="https://twitter.com/ufc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            X (Twitter)
+          </a>
+        </p>
+      </div>
 
-        <div>
-            <h3>Follow Us</h3>
-            <p>
-                <a href="#" target="_blank">Youtube</a><br/>
-                <a href="#" target="_blank">Facebook</a><br/>
-                <a href="#" target="_blank">Twitter</a>
-            </p>
-        </div>
-        <div>
-            <h3>Contact</h3>
-            <p>
-                email: <a href="mailto:info@xxxxxxxx.com">info@xxxxxxxx.com</a><br/>
-                phone: +1 xxx xxx xxxx<br/>
-                fax: xxx-xxx-xxxx
-            </p>
-        </div>
-        <div>
-            <h3>Correspondence</h3>
-            <address>
-                xxxxxxxxx<br/>
-                P.O.Box 5555<br/>
-                Brooklyn, NY 11201
-            </address>
-            <details>
-                <summary>&copy; 2024 Fighter Finder.</summary>
-                <p>All Rights Reserved.</p>
-            </details>
-        </div>
-      </footer>
-    )
+      <div>
+        <h3>Project Info</h3>
+        <p>
+          This is a front-end demo project built for educational purposes.
+          <br />
+          Data is provided via a mock API.
+        </p>
+      </div>
+
+      <div>
+        <h3>About</h3>
+        <address>
+          Built by <strong>Sitong (Nick) Ren</strong>
+          <br />
+          Software Engineer
+        </address>
+        <details>
+          <summary>&copy; 2026 UFC Champions Hub</summary>
+          <p>All rights reserved.</p>
+        </details>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
